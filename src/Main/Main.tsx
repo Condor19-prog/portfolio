3ff26c1232370ddacf12d95a39ec769212fc1296
yauheni <yauheniauchynnikau19@gmail.com>
2021-03-10 20:56:36 +0300

    processed...4

--- src/Main/Main.tsx
+++ src/Main/Main.tsx
@@ -1,14 +1,15 @@
 import React from "react";
-import style from './Main.module.css'
+import style from './Main.module.scss'
 import styleContainer from '../common/styles/Container.module.css'
 
 const Main = () => {
     return (
         <div className={style.mainBlock}>
-            <div className={styleContainer.container}>
+            <div className={`${styleContainer.container} ${style.mainContainer}`}>
                 <div className={style.text}>
                     <span>Hi There</span>
-                    <h1>I am Yauheni Auchynnikau</h1>
+                    <h1>I am <br/>
+                        Yauheni Auchynnikau</h1>
                     <p>Frontend Developer</p>
                 </div>
                 <div className={style.photo}></div>
